72ceddbf79a2fa6c26ad1631cb81c8ac1fc3c272
Joan López de la Franca Beltran <joanjan14@gmail.com>
2025-05-06 16:03:52 +0200

    Simple k6 and k6/browser script with basic check

---
+++ script.js
@@ -0,0 +1,38 @@
+import {browser} from 'k6/browser';
+import {check} from 'k6';
+
+export const options = {
+	thresholds: {
+		checks: ['rate==1.0'],
+	},
+	scenarios: {
+		default: {
+			executor: 'shared-iterations',
+			options: {
+				browser: {
+					type: 'chromium',
+				},
+			},
+		},
+	},
+};
+
+export default async function () {
+	const context = await browser.newContext();
+	const page = await context.newPage();
+
+	try {
+		// Navigate to the site
+		await page.goto('https://joanlopez.github.io/terraform-k6-example/');
+
+		// Get the page content
+		const content = await page.content();
+
+		// Check it contains the title
+		check(content, {
+			'the title is displayed': (html) => html.includes('Terraform k6 example'),
+		});
+	} finally {
+		await page.close();
+	}
+}
